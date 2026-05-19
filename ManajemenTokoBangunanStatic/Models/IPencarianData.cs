d95f64fdc5f0a3bb665498df3eb35a30b721cf00
dynastygunawan22 <dynastygunawan22@gmail.com>
2026-05-19 14:02:47 +0700

    menambahkan revisi generic berbasis interface

---
+++ ManajemenTokoBangunanStatic/Models/IPencarianData.cs
@@ -0,0 +1,10 @@
+﻿namespace ManajemenTokoBangunanStatic.Models
+{
+    // REVISI:
+    // Interface ini dipakai supaya generic punya standar field pencarian.
+    // TEKNIK KONSTRUKSI: Parameterization / Generics berbasis interface.
+    public interface IPencarianData
+    {
+        string TeksPencarian { get; }
+    }
+}
